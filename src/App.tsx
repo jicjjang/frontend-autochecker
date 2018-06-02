56ac061166804be7f7b8a90b40123b82218f8a8d
jicjjang <jicjjang12@gmail.com>
2018-06-02 21:04:56 +0900

    Basic settings for dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
+export default class App extends React.Component<{}, {}> {
+  public render(): React.ReactElement<any> {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{`Frontend Autochecker`}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
+        
+
+        <footer className="App-intro">
+          hohoho!
+        </footer>
       </div>
     );
   }
 }
-
-export default App;
